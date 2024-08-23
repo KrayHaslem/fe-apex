00ca199c767f451418d8eff01de613bc76491f51
Kray Haslem <kray@devpipeline.com>
2024-08-22 22:20:33 -0600

    adds HomePage and LoginPage and brings in react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import LoginPage from "./components/pages/LoginPage";
+import HomePage from "./components/pages/HomePage";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
   );
 }
 
