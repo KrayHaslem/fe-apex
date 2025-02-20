d717e56850ca6c5dbce1639c891ff13a9e3fb0b9
Kray Haslem <kray@devpipeline.com>
2025-02-20 13:52:53 -0700

    navigate instead of history

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const submit = (event) => {
     event.preventDefault();
@@ -27,7 +27,7 @@ export default function LoginPage() {
         if (!data.ok) {
           setError(data);
         } else {
-          history.push("/home");
+          navigate.push("/home");
         }
       })
       .catch((err) => {
